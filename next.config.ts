881f646208364e63d1ddb07f0eb0c2ec93db162e
Paul Priddy <paul@example.com>
2025-12-17 17:19:03 -0500

    Force update: New DB route and config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   typescript: {
-    // ⚠️ Warning: This allows production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
