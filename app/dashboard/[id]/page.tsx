2174af842ebd7064a15df06ceed7261f21685ed9
Paul Priddy <paul@example.com>
2025-12-17 17:16:18 -0500

    Move dashboard to new route to fix caching

--- app/dashboard/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function DynamicDashboard() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
-      <h1 className="text-3xl font-bold">Dashboard Updating...</h1>
-    </div>
-  );
-}
